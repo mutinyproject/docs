7e09469182f6b60496a61a4bf15e33a4f24658fb
Kylie McClain <kylie@somas.is>
2020-07-04 17:30:07 -0400

    site.sh: allow for specifying combines as part of environment

--- site.sh
+++ site.sh
@@ -5,7 +5,11 @@ set -ex
 rm -rf site
 mkdir -p site
 
-COMBINES="idioms filesystem praxis"
+: "${COMBINES:=
+    https://git.mutiny.red/mutiny/idioms
+    https://git.mutiny.red/mutiny/filesystem
+    https://git.mutiny.red/mutiny/praxis
+}"
 
 while [ $# -gt 0 ]; do
     printf 'ASCIIDOCTOR_FLAGS += "%s"\n' "$1" >> site/config.mk
@@ -13,19 +17,22 @@ while [ $# -gt 0 ]; do
 done
 
 for d in ${COMBINES}; do
-    git clone --depth=1 https://git.mutiny.red/mutiny/"${d}" site/"${d}"
-    touch site/"${d}"/config.mk
-    [ -f ./config.mk ] && cat ./config.mk > site/"${d}"/config.mk
-    ln -sf ../config.mk site/"${d}"/config.mk
+    name="${d##*/}"; name="${d%%.git}"
+    git clone --depth=1 "${d}" site/"${name}"
+    touch site/"${name}"/config.mk
+    [ -f ./config.mk ] && cat ./config.mk > site/"${name}"/config.mk
+    ln -sf ../config.mk site/"${name}"/config.mk
 done
 
 make clean
 
 for d in ${COMBINES}; do
-    make -C site/"${d}" html
+    name="${d##*/}"; name="${d%%.git}"
+    make -C site/"${name}" html
 done
 
-for d in ../ ${COMBINES}; do
-    make -C site/"${d}" install-html DESTDIR="${PWD}"/site/generated htmldir=/
+for d in .. ${COMBINES}; do
+    name="${d##*/}"; name="${d%%.git}"
+    make -C site/"${name}" install-html DESTDIR="${PWD}"/site/generated htmldir=/
 done
 
