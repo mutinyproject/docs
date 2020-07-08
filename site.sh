30ba189fb16d3f0381a8462344197782b1b6e1fd
Kylie McClain <kylie@somas.is>
2020-07-08 01:16:11 -0400

    site.sh: uncomplicate a little bit

--- site.sh
+++ site.sh
@@ -11,22 +11,20 @@ mkdir -p site
     https://git.mutiny.red/mutiny/praxis
 }"
 
+for d in ${COMBINES}; do
+    name="${d##*/}"; name="${name%%.git}"
+    git clone --depth=1 "${d}" site/"${name}"
+    touch site/"${name}"/config.mk
+    [ -f ./config.mk ] && cat ./config.mk >> site/"${name}"/config.mk
+    ln -sf ../config.mk site/"${name}"/config.mk
+done
+
 while [ $# -gt 0 ]; do
     printf 'ASCIIDOCTOR_FLAGS += "%s"\n' "$1" >> site/config.mk
     shift
 done
 
-for d in ${COMBINES}; do
-    name="${d##*/}"; name="${name%%.git}"
-    git clone --depth=1 "${d}" site/"${name}"
-    touch site/"${name}"/config.mk
-    [ -f ./config.mk ] && cat ./config.mk > site/"${name}"/config.mk
-    ln -sf ../config.mk site/"${name}"/config.mk
-done
-
-make clean
-
-for d in ${COMBINES}; do
+for d in .. ${COMBINES}; do
     name="${d##*/}"; name="${name%%.git}"
     make -C site/"${name}" html
 done
